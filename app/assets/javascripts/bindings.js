b69f82b4b6a108d9cd805a7f3027c8345a63f297
benoit <benoit.maizi@polytechnique.edu>
2021-02-07 19:20:44 +0100

    added support for laptop digits

--- app/assets/javascripts/bindings.js
+++ app/assets/javascripts/bindings.js
@@ -1,6 +1,6 @@
 var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
                'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']; // eslint-disable-line indent
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'shift+0', 'shift+1', 'shift+2', 'shift+3', 'shift+4', 'shift+5', 'shift+6', 'shift+7', 'shift+8', 'shift+9'];
 
 function inputFocused() {
   return document.activeElement.tagName === 'INPUT';
